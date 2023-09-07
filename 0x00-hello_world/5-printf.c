824323b035e4da1cc43b444e40776b011b59dea0
youcef <youcefmadridano@gmail.com>
2023-09-07 13:54:01 -0700

    555

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ *main - do a printf
+ *
+ *Description: a func to print a text
+ *Return: a 0 always
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
