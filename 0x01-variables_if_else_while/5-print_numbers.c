5ce93fd11c83a8adb2b900f2ab7cb014b71de623
youcef <youcefmadridano@gmail.com>
2023-09-08 12:17:04 -0700

    000

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
-
+/**
+  * main - a func
+  *
+  * Description: a function
+  * Return: a 0
+  */
 
 int main(void)
 {
 	char start = '0';
 	int i;
+
 	for (i = 0 ; i < 10; i++)
 	{
 		putchar(start + i);
