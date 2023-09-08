3969081da1cdfff426859ceedacfe2a3b02e292b
youcef <youcefmadridano@gmail.com>
2023-09-08 12:02:00 -0700

    adsf

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+  * main - function
+  *
+  * Descriptiot: a func
+  * Return: a 0
+  */
+int main(void)
+{
+	char start = 'a';
+	int i;
+
+	for (i = 0; i < 26; i++)
+ 	{
+		if ((start + i) != 'e')
+			if ((start + i) != 'q')
+				putchar(start + i);
+	}
+ 	putchar('\n');
+	return (0);
+}
