a1cd285bddbac04c445de78c9ca6f0bbb9c6c3c8
youcef <youcefmadridano@gmail.com>
2023-09-08 11:41:29 -0700

    hh

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
-
+/**
+  * main - function
+  *
+  * Descriptiot: a func
+  * Return: a 0
+  */
 int main(void)
 {
 	char start = 'a';
 	int i;
+
 	for (i = 0; i < 26; i++)
 	{
 		putchar(start + i);
