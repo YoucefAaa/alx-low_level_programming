bfa97076f287f26d636e3ab7195583271fb7b8bf
youcef <youcefmadridano@gmail.com>
2023-09-08 13:43:54 -0700

    at

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+  * main - a func
+  *
+  * Description: af aop
+  * Return: a 0
+  */
+
+int main(void)
+{
+	int z = '0';
+	int i;
+	for (i = 0 ; i <= 9 ; i++)
+	{
+		putchar(z + i);
+	}
+	putchar('\n');
+	return (0);
+}
