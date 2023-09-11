6a7f9b3b01fbf78c322fad14140252e6a241370e
youcef <youcefmadridano@gmail.com>
2023-09-11 11:51:07 -0700

    adfk

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,12 @@
 #include<unistd.h>
 /**
+  * main - a func
   *
+  * Description: a function to do stuff
+  * Return: a 1 error
   */
 int main() 
 { 
-	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\r\n", 11); 
-	return (0); 
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 11); 
+	return (1); 
 } 
