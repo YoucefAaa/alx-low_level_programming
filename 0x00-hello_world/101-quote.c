ca5823fa06828f3d950be7aaa71fefb9cada3a77
youcef <youcefmadridano@gmail.com>
2023-09-11 11:55:08 -0700

    adsfj

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,7 +5,7 @@
   * Description: a function to do stuff
   * Return: a 1 error
   */
-int main()
+int main(void)
 {
 	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
