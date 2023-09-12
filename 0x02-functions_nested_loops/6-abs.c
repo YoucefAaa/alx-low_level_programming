0d8c01d2195bfdf57d5c1fca990a109bc1f09535
youcef <youcefmadridano@gmail.com>
2023-09-12 10:39:07 -0700

    asfe

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _abs - a func
+ *
+ * @n : the abs
+ * Description: asdf
+ * Return: asf
+ */
+
+int _abs(int n)
+{
+	_putchar(abs(n))
+	return (0)
+}
