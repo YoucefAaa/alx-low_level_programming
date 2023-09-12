ef356c8c2d2b6727a2dcd6f27f7804f24f56b3b9
youcef <youcefmadridano@gmail.com>
2023-09-12 09:43:36 -0700

    asdft

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * main - a func
+ *
+ * Description: a good alphabet printer
+ * Return: a 0
+*/
+
+
+void print_alphabet(void)
+{
+	int i;
+	int j;
+	char start = 'a';
+
+	for (j = 0 ; j < 10; j++)
+	{
+		for (i = 0; i < 26; i++)
+		{
+			_putchar(start + i);
+		}
+	}
+	_putchar('\n');
+}
+
+
