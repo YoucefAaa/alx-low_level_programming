2bc0dda8b778959876c1e6013a78e81190f42fcb
youcef <youcefmadridano@gmail.com>
2023-09-12 12:01:01 -0700

    asr

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+  * print_last_digit - a func
+  * @n : the char
+  * Description: fg
+  * Return: the last digit of a number
+  */
+
+int print_last_digit(int n)
+{
+	int y = n % 10;
+
+	return (y);
+}
