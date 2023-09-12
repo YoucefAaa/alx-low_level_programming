f2e1886e943133c73bf7b1cdc0fe82dae24b619d
youcef <youcefmadridano@gmail.com>
2023-09-12 13:34:35 -0700

    to hard

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main"
+/**
+  * add - a func
+  * @a : a var
+  * @b : another ver
+  *
+  * Description: holla amigo
+  * Return: a indt
+  */
+
+int add(int b, int a)
+{
+	int c = a + b;
+
+	return (c);
+}
---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,29 @@
+#include "main"
+/**
+ * jack_bauer - a boring long code
+ *
+ * Description: a good code
+ */
+
+void jack_bauer(void)
+{
+	int i, j;
+
+	i = 0;
+
+	while (i < 24)
+	{
+		j = 0;
+		while (j < 60)
+		{
+			_putchar((i / 10) + '0');
+			_putchar((i % 10) + '0');
+			_putchar(':');
+			_putchar((j / 10) + '0');
+			_putchar((j % 10) + '0');
+			_putchar('\n');
+			j++;
+		}
+		i++;
+	}
+}
