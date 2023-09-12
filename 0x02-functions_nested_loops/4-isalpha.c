84c8b57e384f733e816f3a835779de17f4e3a949
youcef <youcefmadridano@gmail.com>
2023-09-12 10:13:56 -0700

    afd

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _isalpha - a func
+ * @c : the char
+ *
+ * Desciption: a good func
+ * Return: 1 if it is lower case and 0 if it is an upper case
+ */
+
+int _isalpha(int c)
+{
+	if (c < 122 && c > 97 && c < 65 && c > 90)
+		return (1);
+	else
+		return (0);
+}
