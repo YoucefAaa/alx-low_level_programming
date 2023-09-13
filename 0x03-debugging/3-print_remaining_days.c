b5c3187a8e31c26faa6e4bab32220407bb89c520
youcef <youcefmadridano@gmail.com>
2023-09-13 10:19:59 -0700

    to

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -12,7 +12,10 @@
 
 void print_remaining_days(int month, int day, int year)
 {
-	if ((year % 4 == 0 && year % 100 != 0) || (year % 4 == 0 && year % 100 == 0 && year % 400 == 0))
+	int leap1 = (year % 4 == 0 && year % 100 != 0);
+	int leap2 = (year % 4 == 0 && year % 100 == 0 && year % 400 == 0);
+
+	if (leap1 || leap2)
 	{
 		if (month >= 2 && day >= 60)
 		{
