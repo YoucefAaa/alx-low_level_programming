79976bcef4efc563651ae94ef7fb05a4fa1e09db
youcef <youcefmadridano@gmail.com>
2023-09-14 07:13:16 -0700

    llp

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+  * _isupper - a func
+  * @c : the char
+  *
+  * Description: a func to determine wethere c is uppercasr or lowercase
+  * Return: a 1 if c is uppercase or c is lowercase
+  */
+
+int _isupper(int c)
+{
+	if (c > 65 && c < 90)
+		return (1);
+	else
+		return (0);
+}
+
