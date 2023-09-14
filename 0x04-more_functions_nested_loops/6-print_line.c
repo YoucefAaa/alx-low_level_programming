2cfe69f52ced058a1a964752d9cdc3139dacb5cd
youcef <youcefmadridano@gmail.com>
2023-09-14 10:05:39 -0700

    it is going to work

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,18 +1,20 @@
-#include "maim.h"
+#include "main.h"
 /**
   *
-  88
   *
   *
   *
-  8
   */
 
 void print_line(int n)
 {
-	while (n > 0)
+	if (n > 0)
 	{
-		_putchar('_');
-		n--;
+		while (n > 0)
+		{
+			_putchar('_');
+			n--;
+		}
 	}
+	_putchar('\n');
 }
