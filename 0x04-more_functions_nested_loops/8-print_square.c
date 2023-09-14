4e83d5a1e020f5237423c2cdad059213cea8fdc6
youcef <youcefmadridano@gmail.com>
2023-09-14 10:25:00 -0700

    asdf'

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+  * print_square - a func
+  * @size : the size of the square
+  *
+  */
+
+void print_square(int size)
+{
+	if (size > 0)
+	{
+		int i = size;
+		int j;
+
+		while (i > 0)
+		{
+			j = size;
+			while (j > 0)
+			{
+				_putchar('#');
+				j--;
+			}
+			i--;
+			_putchar('\n');
+		}
+	}
+	_putchar('\n');
+}
