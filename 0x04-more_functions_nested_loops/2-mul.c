92bd32ea714e33ce14cbc637bbe64103e3b40cfc
youcef <youcefmadridano@gmail.com>
2023-09-14 07:33:36 -0700

    add the mul code and the prototype to main.h

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+  * mul - a func to return the mulitipation of to numbers
+  * @a : num 1
+  * @b : num 2
+  *
+  * Return: the result of multi of two numbers
+  */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
