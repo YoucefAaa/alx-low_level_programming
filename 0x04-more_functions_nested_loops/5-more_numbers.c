c242657bb2837d4c0bb6d59ac7fe97d20644998a
youcef <youcefmadridano@gmail.com>
2023-09-15 07:40:40 -0700

    asdfr

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,19 +1,28 @@
 #include "main.h"
+/**
+ * more_numbers - a func
+ *
+ */
 
 void more_numbers(void)
 {
-    int i, j;
+	int i, j, k;
+	char start = '0';
+	char st = '0';
 
-    for (i = 0; i <= 9; i++) 
-    {
-        for (j = 0; j <= 14; j++)
-        {
-		if (j < 10)
-            		_putchar(j + '0');
-		else
-			_putchar(j);
-        }
-        _putchar('\n');
-    }
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			_putchar(start + j);
+		}
+
+		for (k = 0; k < 5; k++)
+		{
+			_putchar('1');
+			_putchar(st + k);
+		}
+
+		_putchar('\n');
+	}
 }
-
