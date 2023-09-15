d9e276019217d873790d2c0abcb17be82075a563
youcef <youcefmadridano@gmail.com>
2023-09-15 09:53:37 -0700

    asdf23

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,21 +1,24 @@
 #include <stdio.h>
-
+/**
+ * main - a func
+ * Return: a 0
+ */
 int main(void)
 {
-    long int number = 612852475143;
-    long int i = 2;
-    while (i * i <= number)
-    {
-        if (number % i == 0)
-        {
-            number /= i;
-        }
-        else
-        {
-            i++;
-        }
-    }
-    printf("%ld\n", number);
-    return (0);
-}
+	long int number = 612852475143;
+	long int i = 2;
+	while (i * i <= number)
+	{
+		if (number % i == 0)
+		{
+			number /= i;
+		}
+		else
+		{
+			i++;
+		}
+	}
 
+	printf("%ld\n", number);
+	return (0);
+}
