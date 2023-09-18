ea7bffffbdc5f9c6b9c4bbaab923ec6f5f1cd5c6
youcef <youcefmadridano@gmail.com>
2023-09-18 19:37:47 +0100

    tui

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -6,11 +6,13 @@
 
 void rev_string(char *s)
 {
-        int i;
+	int i;
 	int lenth = strlen(s);
+
 	for (i = 0 ; i < lenth / 2 ; i++)
 	{
 		char temp = s[i];
+
 		s[i] = s[lenth - i - 1];
 		s[lenth - i - 1] = temp;
 	}
