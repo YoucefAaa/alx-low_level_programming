cc3547a4729a19782377c071f71691b73ca5ddeb
youcef <youcefmadridano@gmail.com>
2023-09-18 19:35:14 +0100

    youi

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -6,5 +6,12 @@
 
 void rev_string(char *s)
 {
-        s = strrev(s);
+        int i;
+	int lenth = strlen(s);
+	for (i = 0 ; i < lenth / 2 ; i++)
+	{
+		char temp = s[i];
+		s[i] = s[lenth - i - 1];
+		s[lenth - i - 1] = temp;
+	}
 }
