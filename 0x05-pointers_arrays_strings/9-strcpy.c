964f7b0c3e08da31e5d84215581894e0498e5860
youcef <youcefmadridano@gmail.com>
2023-09-19 03:31:34 -0700

    toyi upoi

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+/**
+  * *_strcpy - a func
+  * @dest : a param
+  * @src : a param
+  * Return: the dest
+  */
+char *_strcpy(char *dest, char *src)
+{
+	strcpy(dest, src);	
+	return (dest);
+}
