77ef0c98a90c958f6b3eacf12a9a7371dfa4c6fc
youcef <youcefmadridano@gmail.com>
2023-09-19 03:02:51 -0700

    toyou

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <string.h>
+/**
+  * puts_half - a func
+  * @str : a param
+  */
+
+void puts_half(char *str)
+{
+	int i;
+	int lenth = strlen(str);
+
+	if (lenth % 2 == 0)
+	{
+		for (i = lenth / 2 ; i <= lenth ; i++)
+			_putchar(str[i]);
+	}
+	else
+	{
+		int n = (lenth - 1) / 2;
+
+		for (i = n ; i <= lenth ; i++)
+			_putchar(str[i]);
+	}
+	_putchar('\n');
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -3,5 +3,7 @@
 
 int _putchar(char c);
 void reset_to_98(int *n);
-void puts2(char *str)
+void puts2(char *str);
+void puts_half(char *str);
+
 #endif
