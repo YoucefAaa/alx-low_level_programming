62341a57a0c44da88b2ee9fd100d99b686ca3eb7
youcef <youcefmadridano@gmail.com>
2023-09-20 11:51:26 -0700

    :afT:

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <string.h>
+/**
+  * _strcmp - a func
+  * @s1 : a param
+  * @s2 : a second param
+  * Return: the st var
+  *
+  */
+
+int _strcmp(char *s1, char *s2)
+{
+	int st = strcmp(s1, s2);
+
+	return (st);
+}
