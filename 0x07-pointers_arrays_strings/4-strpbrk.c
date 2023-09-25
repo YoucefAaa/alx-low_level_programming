b80fda58064be619475483b6f4fc163fd422b977
youcef <youcefmadridano@gmail.com>
2023-09-25 10:37:59 -0700

    llopol

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,12 @@
+#include <string.h>
+
+/**
+ *_strpbrk - a func
+ *@s : a param
+ *@accept : a param
+ *Return: a text
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	return (strpbrk(s, accept));
+}
