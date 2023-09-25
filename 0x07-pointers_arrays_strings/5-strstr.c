2c8e8c8186385c0983dc4c8733f542e8a9ef2ec8
youcef <youcefmadridano@gmail.com>
2023-09-25 10:40:35 -0700

    poio

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,12 @@
+#include <string.h>
+
+/**
+ *_strstr - a func
+ *@haystack : a param
+ *@needle : a param
+ *Return: a text
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	return (strstr(haystack, needle));
+}
