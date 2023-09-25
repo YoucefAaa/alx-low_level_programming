ea1fc7fd9241d7016d12950e3c808cb078c1814e
youcef <youcefmadridano@gmail.com>
2023-09-25 10:12:48 -0700

    :asdfl

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,11 +1,10 @@
 #include <string.h>
 /**
-  *
-  *
-  *
-  *
-  *
-  *
+  * _memset - a func
+  * @s : a param
+  * @b : to replace
+  * @n : to count
+  * Return: a string
   */
 
 char *_memset(char *s, char b, unsigned int n)
