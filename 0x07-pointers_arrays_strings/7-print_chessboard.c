b61fed1f957ec2bfb7f1231b3219167ea2a3cd43
youcef <youcefmadridano@gmail.com>
2023-09-25 11:20:25 -0700

    :vvnbie

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -5,10 +5,10 @@
   */
 void print_chessboard(char (*a)[8])
 {
-	for (int i = 0; i < 8; i++)
+	for (int i = 0 ; i < 8 ; i++)
 	{
-		for (int j = 0; j < 8; j++)
-			putchar(a[i][j]);
-	putchar('\n');
+		for (int j = 0 ; j < 8 ; j++)
+			_putchar(a[i][j]);
+	_putchar('\n');
 	}
 }
