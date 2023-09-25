c43a2e2e98b2d6060458e1b1f851f3f3f71c8559
youcef <youcefmadridano@gmail.com>
2023-09-25 10:34:44 -0700

    asft

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,11 @@
+#include <string.h>
+/**
+ *_strspn - a func
+ *@s : a param
+ *@accept : a param
+ *Return: a text
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	return (strspn(s, accept));
+}
