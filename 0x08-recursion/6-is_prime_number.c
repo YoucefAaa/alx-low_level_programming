1525308b2563805509bc177d70fcfaded328f69f
youcef <youcefmadridano@gmail.com>
2023-09-29 11:24:13 -0700

    llkl

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,17 +1,15 @@
-
-
 int rer(int x, int y)
 {
-    if (n <= 1)
+    if (x <= 1)
         return 0;
-    if (i == 1)
+    if (x == 1)
         return 1;
     else
     {
-        if (n % i == 0)
+        if (x % i == 0)
             return 0;
         else
-            return is_prime(n, i - 1);
+            return is_prime(x, i - 1);
     }
 }
 int is_prime_number(int n)
